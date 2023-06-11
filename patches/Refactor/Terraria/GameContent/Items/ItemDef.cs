5421b73d57efa570754830f96ca867b4d44b8b00
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-06-11 18:11:23 -0500

    Started Refactoring Items

---
+++ patches/Refactor/Terraria/GameContent/Items/ItemDef.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria.Audio;
+using Terraria.ID;
+
+namespace Terraria.GameContent.Items
+{
+	public class ItemDef
+	{
+		public readonly int itemID;
+		public int useStyle;
+		public bool useTurn;
+		public int useAnimation;
+		public int useTime;
+		public bool autoReuse;
+		public int width;
+		public int height;
+		public int damage;
+		public int pick;
+		public LegacySoundStyle UseSound;
+		public float knockBack;
+		public int value;
+		public bool melee;
+
+		public ItemDef(int id)
+		{
+			itemID = id;
+			_items.Add(id, this);
+		}
+
+		private static Dictionary<int, ItemDef> _items = new();
+		private static bool _initialized;
+
+		public static ItemDef Get(int id)
+		{
+			InitItems(); // TODO: Call this in a better place
+
+			if (_items.ContainsKey(id))
+				return _items[id];
+
+			return null;
+		}
+
+		public static ItemIronPickaxe ironPickaxe;
+
+		public static void InitItems()
+		{
+			if (_initialized)
+				return;
+
+			_initialized = true;
+
+			ironPickaxe = new ItemIronPickaxe();
+		}
+	}
+}
---
+++ patches/Refactor/Terraria/GameContent/Items/ItemIronPickaxe.cs
@@ -0,0 +1,28 @@
+﻿using Terraria.ID;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Terraria.GameContent.Items
+{
+	public class ItemIronPickaxe : ItemDef
+	{
+		public ItemIronPickaxe() : base(ItemID.IronPickaxe)
+		{
+			useStyle = 1;
+			useTurn = true;
+			useAnimation = 20;
+			useTime = 13;
+			autoReuse = true;
+			width = 24;
+			height = 28;
+			damage = 5;
+			pick = 40;
+			UseSound = SoundID.Item1;
+			knockBack = 2f;
+			value = 2000;
+			melee = true;
+		}
+	}
+}
