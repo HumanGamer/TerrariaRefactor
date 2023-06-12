400eac760d42c5cb1ca8607c3f0d5bc3682853e7
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-06-12 18:52:43 -0500

    Started working on Movement Core item for mod

---
+++ patches/MattTerrariaMod/Terraria/GameContent/Items/ItemMovementCore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria.Enums;
+using Terraria.ID;
+
+namespace Terraria.GameContent.Items
+{
+	public class ItemMovementCore : ItemDef
+	{
+		public ItemMovementCore() : base(ItemID.MovementCore)
+		{
+			width = 34;
+			height = 30;
+			accessory = true;
+			shoeSlot = 24;
+			rare = (int)ItemRarityColor.Lime7;
+			value = Item.sellPrice(0, 15);
+			hasVanityEffects = true;
+		}
+	}
+}
